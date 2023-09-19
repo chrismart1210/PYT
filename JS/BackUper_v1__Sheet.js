ece65a293e214df3f1c20e1168838ac9ea5e69b3
Chris <108366775+chrismart1210@users.noreply.github.com>
2023-09-19 14:03:12 -0300

    Create BackUper_v1__Sheet.js

---
+++ JS/BackUper_v1__Sheet.js
@@ -0,0 +1,31 @@
+function BackUper_v1__Sheet()
+{
+  // INPUTs
+    // Origen 
+      var Book = "ID" ;
+
+      var Sheet_origen = SpreadsheetApp.openById(Book).getSheetByName("Export Sheet_WH");
+        var Upload_Row = 2;
+        var Upload_Col = 1;
+    
+
+    // Destino
+      var Sheet_destino = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Import Sheet_WH");
+        var Download_Row = 2;
+        var Download_Col = 1;
+
+
+  // TTT
+    var data = Sheet_origen.getRange(Upload_Row, Upload_Col, Sheet_origen.getLastRow() - Upload_Row + 1, Sheet_origen.getLastColumn() - Upload_Col + 1);
+
+    
+  // OUTPUTs
+    // Setting
+      Sheet_destino.getRange(Download_Row, Download_Col, data.length, data[0].length).setValues(data);
+
+    // Time-Stamp
+      Sheet_destino.getRange(1, 1).setValue(Utilities.formatDate(new Date(), "GMT-3", "dd/MM - HH:mm"));  
+      Sheet_destino.getRange(1, 1).setBackground("black");
+      Sheet_destino.getRange(1, 1).setFontColor("#00FF00");
+      Sheet_destino.getRange(1, 1).setFontWeight("bold");
+}
